19cfac4ac217918c92e34888c1008b2429604f7d
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2024-07-13 17:39:30 +0800

    setup livewire

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="" />
+    <title>{{ $title ?? 'Page Title' }} &mdash; {{ config('app.name') }}</title>
+</head>
+
+<body>
+    <main>
+        @if (isset($header))
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+        @endif
+        {{ $slot }}
+    </main>
+</body>
+
+</html>
