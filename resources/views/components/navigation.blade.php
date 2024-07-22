b2080fb1dba49ccf19b4d48b155ca92a5e9b7f8b
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2024-07-22 19:27:46 +0800

    remove wire navigate

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,11 +1,11 @@
 <nav class="flex flex-row gap-x-4">
     <div>
         <a href="{{ route('welcome') }}" class="nav-menu {{ request()->routeIs('welcome') ? 'active-menu' : '' }}"
-            wire:navigate @if (request()->routeIs('welcome')) aria-current="page" @endif>Beranda</a>
+            @if (request()->routeIs('welcome')) aria-current="page" @endif>Beranda</a>
     </div>
     <div>
         <a href="{{ route('attachments') }}" class="nav-menu {{ request()->routeIs('attachments') ? 'active-menu' : '' }}"
-            wire:navigate @if (request()->routeIs('attachments')) aria-current="page" @endif>Lampiran</a>
+            @if (request()->routeIs('attachments')) aria-current="page" @endif>Lampiran</a>
     </div>
     <div>
         <a href="{{ env('WEB_PORTAL_URL') }}" class="nav-menu">Web Portal</a>
