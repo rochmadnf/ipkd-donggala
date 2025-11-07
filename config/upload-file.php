5e9c40f13ca911ba349eb9134fbfc2254a3b57a1
rochmadnf <rochmadnf@gmail.com>
2025-11-07 13:00:09 +0800

    set key when upload file

---
+++ config/upload-file.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'key' => env('UPLOAD_FILE_RANDOM_KEY', 'XxWXg2ifQNzSXJY6'),
+];
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,9 @@ Route::get('/', [Controllers\WelcomeController::class, 'index'])->name('welcome'
 Route::get('/attachments', [Controllers\AttachmentController::class, 'index'])->name('attachments');
 
 Route::controller(Controllers\UploadFileController::class)->prefix('uploads')->group(function () {
-    Route::get('/', 'index')->name('index.file');
-    Route::post('/', 'store')->name('store.file');
+
+    $randKey = config('upload-file.key');
+
+    Route::get("/key=$randKey", 'index')->name('index.file');
+    Route::post("/key=$randKey", 'store')->name('store.file');
 });
